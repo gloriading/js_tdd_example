69db627869b5f3bc022a2777cab994dfcdccb2fb
Gloria Ding <gloria.hc.ding@gmail.com>
2019-07-22 20:09:52 -0700

    The second test is passed for the current func

--- index.test.js
+++ index.test.js
@@ -4,4 +4,8 @@ describe('fizzBuzz', () => {
   it('returns 1 when the input is 1', () => {
     expect(fizzBuzz(1)).toBe(1);
   });
+
+  it('returns 2 when the input is 2', () => {
+    expect(fizzBuzz(2)).toBe(2);
+  });
 });
