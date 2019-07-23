f2f064f5df86b518cc0b395f3e653bd747906e7d
Gloria Ding <gloria.hc.ding@gmail.com>
2019-07-22 20:31:38 -0700

    Refactor the code with confidence!

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function fizzBuzz(num) {
-  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
-  if (num % 3 === 0) return 'Fizz';
-  if (num % 5 === 0) return 'Buzz';
+  let string = '';
+  if (num % 3 === 0) string += 'Fizz';
+  if (num % 5 === 0) string += 'Buzz';
 
-  return num;
+  return string.length ? string : num;
 }
 
 module.exports = fizzBuzz;
